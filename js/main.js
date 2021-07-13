93bfaa1e976277f51d9d82e94f78ed6503c950a1
ahmedloll <ahmed.alaa.fathy.96@gmail.com>
2021-07-13 19:35:52 +0200

    added all pages

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// toogle side menu
+document.getElementById("menu").addEventListener("click", showSideMenu);
+function showSideMenu() {
+  let disply = document.querySelector("nav ").classList.contains("appear");
+  if (!disply) {
+    document.querySelector("nav").classList.add("appear");
+  } else {
+    document.querySelector("nav").classList.remove("appear");
+  }
+}
+
+// close menus when clicked outside
+window.addEventListener("click", function (e) {
+  let side_menu = document.querySelector("nav");
+  if (
+    !side_menu.contains(e.target) &&
+    e.target != document.querySelector("#menu i")
+  ) {
+    document.querySelector("nav").classList.remove("appear");
+  }
+});
